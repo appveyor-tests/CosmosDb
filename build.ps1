7bedb3429ab8058b798ee93465e29aab84842fe1
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-10-25 06:35:39 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
-    Set-Content -Value '"C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe" /NoUI /NoExplorer /NoFirewall' -Path .\startCosmosDb.cmd
+    Set-Content -Value '"C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe" /NoUI /NoExplorer /NoFirewall /Port=8181' -Path .\startCosmosDb.cmd
     Start-Process -FilePath .\startCosmosDb.cmd
-    .\waitforport.ps1 -port 8081 -maxAttempts 4
+    .\waitforport.ps1 -port 8181 -maxAttempts 4
